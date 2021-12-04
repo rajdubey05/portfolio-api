ed59f18e5e9a7addca69b1b487c484b7b901ee84
rajdubey05 <99rajdubey@gmail.com>
2021-12-04 13:53:26 +0530

    ..

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const port = "5000";
 
 const userRouter = require("./routers/userRouter");
 const portfolioRouter = require("./routers/portfolioRouter");
+const utilsRouter = require("./routers/utils")
 
 app.use(express.json());
 app.use(cors({ origin: ["http://localhost:3000"] }));
+
+app.use(express.static('./uploads'));
+
 app.use("/user", userRouter);
 app.use("/portfolio", portfolioRouter);
+app.use("util", utilsRouter);
 
 app.listen(port, () => {
   console.log(" server successfully started on port 5000");
